ee8aca77f736a16acb25a6dcaabbfa11b199a994
DoodlesEpic <doodles33@protonmail.com>
2022-05-14 23:48:40 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>Welcome to {name}!</h1>
+  <p>
+    Use this application to generate prime sequences and graph them all within
+    the comfort of your desktop.
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
