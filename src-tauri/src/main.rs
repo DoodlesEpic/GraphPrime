24a971ed6604c1b2f070e4828e6897eddd90c441
DoodlesEpic <doodles33@protonmail.com>
2022-05-16 21:01:21 -0300

    Sieve of Eratosthenes implementation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,23 +12,24 @@ fn main() {
 
 #[tauri::command]
 async fn calculate(x: u64) -> Vec<u64> {
-    let mut result = vec![2];
+    // Define an array of Boolean values, index from 2 to n, initially all set to true.
+    let mut is_prime = vec![true; (x + 1) as usize];
 
-    // Only iterate over odd numbers since 2 is the only even prime
-    for i in (3..x).step_by(2) {
-        if is_prime(i) {
-            result.push(i);
-        }
-    }
-
-    result
-}
-
-fn is_prime(x: u64) -> bool {
+    // Sieve of Eratosthenes
     for i in 2..x {
-        if x % i == 0 {
-            return false;
+        if is_prime[i as usize] {
+            for j in (i * i..=x).step_by(i as usize) {
+                is_prime[j as usize] = false;
+            }
         }
     }
-    return true;
+
+    // Return all prime numbers in the array
+    is_prime
+        .iter()
+        .enumerate()
+        .skip(2)
+        .filter(|(_, &is_prime)| is_prime)
+        .map(|(i, _)| i as u64)
+        .collect()
 }
