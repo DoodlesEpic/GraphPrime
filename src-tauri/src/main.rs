a60d58d1f55124733eba8ee671bcb688131f34df
DoodlesEpic <doodles33@protonmail.com>
2022-05-16 21:48:21 -0300

    Reimplement calculate using slow_primes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use slow_primes;
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![calculate])
@@ -12,24 +14,6 @@ fn main() {
 
 #[tauri::command]
 async fn calculate(x: u64) -> Vec<u64> {
-    // Define an array of Boolean values, index from 2 to n, initially all set to true.
-    let mut is_prime = vec![true; (x + 1) as usize];
-
-    // Sieve of Eratosthenes
-    for i in 2..x {
-        if is_prime[i as usize] {
-            for j in (i * i..=x).step_by(i as usize) {
-                is_prime[j as usize] = false;
-            }
-        }
-    }
-
-    // Return all prime numbers in the array
-    is_prime
-        .iter()
-        .enumerate()
-        .skip(2)
-        .filter(|(_, &is_prime)| is_prime)
-        .map(|(i, _)| i as u64)
-        .collect()
+    let sieve = slow_primes::Primes::sieve(x as usize);
+    sieve.primes().map(|p| p as u64).collect()
 }
