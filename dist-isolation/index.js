aa45a890d9d2e33a40fe53bdf1f9c10cf29a9d95
Doodles <moraes.eduardo@proton.me>
2025-07-17 12:31:15 -0300

    fix: handle new tauri v2 payload in isolation application

--- dist-isolation/index.js
+++ dist-isolation/index.js
@@ -11,7 +11,7 @@ window.__TAURI_ISOLATION_HOOK__ = (payload) => {
     // Guarantee that the hook has a "callback", "x", "cmd", and "error" property
     if (
       !("callback" in payload) ||
-      !("x" in payload) ||
+      !("x" in payload.payload) ||
       !("cmd" in payload) ||
       !("error" in payload)
     ) {
@@ -20,7 +20,7 @@ window.__TAURI_ISOLATION_HOOK__ = (payload) => {
 
     // Gurantee that x, error, and callback are numbers, and that cmd is a string
     if (
-      typeof payload.x !== "number" ||
+      typeof payload.payload.x !== "number" ||
       typeof payload.error !== "number" ||
       typeof payload.callback !== "number" ||
       typeof payload.cmd !== "string"
@@ -29,7 +29,7 @@ window.__TAURI_ISOLATION_HOOK__ = (payload) => {
     }
 
     // Guarantee that "x" is a positive number
-    if (payload.x <= 0) {
+    if (payload.payload.x <= 0) {
       console.error("x is not a positive number");
       throw new Error("Hook has invalid values");
     }
